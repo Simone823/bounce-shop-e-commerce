162cfadf492b71dfe6fb880fceecd2ebae8cc6ac
Simone823 <simone.daglio823@outlook.it>
2022-07-18 13:38:06 +0200

    modifica fillable model user

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'surname', 'city', 'address', 'email', 'password',
     ];
 
     /**
