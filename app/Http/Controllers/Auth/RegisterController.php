668a1ff9ebfd6c72a00db4f9706762f086fca2f4
Simone823 <simone.daglio823@outlook.it>
2022-07-18 13:46:02 +0200

    register controller user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -50,9 +50,13 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'min:3', 'max:100'],
+            'surname' => ['required', 'string', 'min:3', 'max:100'],
+            'city' => ['required', 'string', 'min:3', 'max:155'],
+            'address' => ['required', 'string', 'min:3', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'image' => ['nullable', 'file', 'image', 'mimetypes:image/jpeg,image/png,image/svg', 'max:2048'],
         ]);
     }
 
