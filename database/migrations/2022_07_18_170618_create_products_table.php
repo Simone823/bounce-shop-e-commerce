8f23043893b46fb46ea9fe6e9f55cf644fef9293
Simone823 <simone.daglio823@outlook.it>
2022-07-18 19:12:04 +0200

    migration product

---
+++ database/migrations/2022_07_18_170618_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name', 250);
+            $table->string('slug', 255)->unique();
+            $table->text('description');
+            $table->float('price', 5,2);
+            $table->string('image')->nullable();
+            $table->boolean('visibility')->default(1);
+            $table->tinyInteger('quantity')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
