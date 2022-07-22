846b1401907433f83ff7403f55a94e481b436678
Simone823 <simone.daglio823@outlook.it>
2022-07-22 18:42:20 +0200

    modifica admin header

--- resources/views/components/adminHeader.blade.php
+++ resources/views/components/adminHeader.blade.php
@@ -7,11 +7,14 @@
                 {{ config('app.name', 'Laravel') }}
             </a>
 
+            {{-- Icon hamburger menu --}}
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                {{-- Links --}}
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link @if(Route::is('admin.home'))active @endif fs-5" aria-current="page" href="{{route('admin.home')}}">Dashboard</a>
