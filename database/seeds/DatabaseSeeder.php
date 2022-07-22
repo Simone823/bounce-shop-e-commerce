637cdf5405930a69309fe09a95efcc13bdcbd555
Simone823 <simone.daglio823@outlook.it>
2022-07-22 15:14:18 +0200

    Revert "Merge branch 'create-migration-products' into development"
    This reverts commit 8edd45bff1f71ed0581a6ff51ccc84ee708a0ff7, reversing
    changes made to 4f52dfb69f83d1fbc95f254eae4bebf8e351a65d.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call([
             LaratrustSeeder::class,
             UserSeeder::class,
-            ProductSeeder::class,
         ]);
     }
 }
