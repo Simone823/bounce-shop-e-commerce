d9829be40b482adbfeedff4db3fc9a09faf9cf62
Simone823 <simone.daglio823@outlook.it>
2022-07-22 12:12:13 +0200

    login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -26,7 +27,19 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+
+    protected function authenticated(Request $request, $user)
+    {
+        if($user->hasRole('superadministrator')) {
+
+            return redirect('/admin/home');
+
+        } else if($user->hasRole('user')) {
+            
+            return redirect('/user/home');
+        }
+    }
+    
 
     /**
      * Create a new controller instance.
