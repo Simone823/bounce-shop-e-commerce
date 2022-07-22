a50eb3b23c4ffa5f39587f96d3ad588360f67964
Simone823 <simone.daglio823@outlook.it>
2022-07-22 16:32:03 +0200

    modifica admin home

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.app')
 
+@section('header')
+
+    @include('components.adminHeader')
+
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
