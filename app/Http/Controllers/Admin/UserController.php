45b264c7cf6e6f866a977c47accfd21f884de4ef
Simone823 <simone.daglio823@outlook.it>
2022-07-22 21:14:44 +0200

    update admin users controller

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Role;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -69,9 +70,16 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(User $user)
     {
-        //
+        // User Auth
+        $user_auth = Auth::user();
+
+        // Roles
+        $roles = Role::all();
+
+        // return view admin users edit
+        return view('admin.users.edit', compact('user_auth', 'user', 'roles'));
     }
 
     /**
@@ -81,9 +89,27 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        //
+        // Request data
+        $data = $request->all();
+
+        // request validate
+        $request->validate([
+            'roles' => 'required|exists:roles,id'
+        ]);
+
+        // Controllo se esiste il ruolo lo aggiorno
+        if (array_key_exists('roles', $data)) {
+            // Sync roles
+            $user->roles()->sync($data['roles']);
+        }
+
+        // Aggiorno i dati
+        $user->update();
+
+        // redirect route admin users show
+        return redirect()->route('admin.users.show', compact('user'));
     }
 
     /**
@@ -94,7 +120,6 @@ class UserController extends Controller
      */
     public function destroy(User $user)
     {
-
         // User delete
         $user->delete();
 
