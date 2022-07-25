428a4a2cee920aa23bac24429262b403d1fcc7e1
Simone823 <simone.daglio823@outlook.it>
2022-07-25 20:24:31 +0200

    slug category seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -2,6 +2,7 @@
 
 use App\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class CategorySeeder extends Seeder
 {
@@ -39,6 +40,15 @@ class CategorySeeder extends Seeder
 
             // assegno i valori
             $new_category->category_name = $category['category_name'];
+
+            // Slug product name
+            $slug = Str::slug($category['category_name']);
+
+            // Slug base
+            $slug_base = $slug;
+
+            // product slug
+            $new_category->slug = $slug_base;
 
             // save
             $new_category->save();
