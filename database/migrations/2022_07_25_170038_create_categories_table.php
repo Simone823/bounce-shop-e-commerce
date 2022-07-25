d7c3d21a73f647ecb384e15bb8998142ee452e69
Simone823 <simone.daglio823@outlook.it>
2022-07-25 20:24:52 +0200

    add slug table migration categories

--- database/migrations/2022_07_25_170038_create_categories_table.php
+++ database/migrations/2022_07_25_170038_create_categories_table.php
@@ -16,6 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('category_name', 250);
+            $table->string('slug', 255)->unique();
             $table->timestamps();
         });
     }
