e9981009d9c3551cdfd01f31ddd34684db9c1bc3
Simone823 <simone.daglio823@outlook.it>
2022-07-26 10:31:56 +0200

    select categories edit

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -83,6 +83,21 @@
                                             @enderror
                                         </div>
 
+                                        {{-- categories --}}
+                                        <div class="categories_select mb-4">
+                                            <label class="mb-2" for="roles">Categoria</label>
+                                            <select name="categories" id="categories" class="form-select mx-auto" aria-label="Default select example">
+                                                <option selected disabled value="">Seleziona una Categoria</option>
+                                                @foreach ($categories as $category)
+                                                    <option {{$product->categories->contains($category) ? 'selected' : ''}} value="{{$category->id}}">{{$category->category_name}}</option>
+                                                @endforeach
+                                            </select>
+
+                                            @error('categories')
+                                            <div class="alert alert-danger">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+
                                         {{-- Image --}}
                                         <div class="form-group mb-3">
                                             <label for="image" class="col-md-4 col-form-label text-md-right">Immagine</label>
