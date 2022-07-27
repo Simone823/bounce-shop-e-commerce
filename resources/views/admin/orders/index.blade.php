650d72ccb82ab7b0c0437911f250b66edafec388
Simone823 <simone.daglio823@outlook.it>
2022-07-27 16:38:14 +0200

    metatitle orders

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.app')
 
+@section('metaTitle', '| Lista Ordini')
+
 @section('header')
     @include('components.adminHeader')
 @endsection
@@ -15,7 +17,7 @@
 
                 {{-- If orders lenght 0 --}}
                 @if(count($orders) < 1)
-                    <div class="no_orders text-center">
+                    <div class="no_orders text-center text-white">
                         <h1>Al momento non ci sono ordini</h1>
                     </div>
                 @endif
