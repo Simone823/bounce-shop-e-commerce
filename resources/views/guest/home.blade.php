8e88cb562edf90f83cafc67b925b68f059f648cc
Simone823 <simone.daglio823@outlook.it>
2022-07-29 15:46:35 +0200

    guest home

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    {{-- Font awesome 6.1.2--}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Styles -->
+    <link href="{{ asset('css/front.css') }}" rel="stylesheet">
+
+    {{-- Meta Title --}}
+    <title>{{config('app.name')}} @yield('metaTitle')</title>
+</head>
+<body>
+
+    {{-- Root --}}
+    <div id="root">
+
+    </div>
+
+    {{-- Script js front --}}
+    <script src="{{asset('js/front.js')}}"></script>
+
+</body>
+</html>
+
