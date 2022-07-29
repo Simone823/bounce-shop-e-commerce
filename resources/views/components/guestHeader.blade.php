5f804b8a915af7771c334382420824a907723544
Simone823 <simone.daglio823@outlook.it>
2022-07-29 17:48:20 +0200

    modifica link menu guest header

--- resources/views/components/guestHeader.blade.php
+++ resources/views/components/guestHeader.blade.php
@@ -16,27 +16,14 @@
 
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                {{-- Link menu --}}
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link fs-5" aria-current="page" href="/">Homepage</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link fs-5" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle fs-5" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
                 </ul>
+
                 <div class="d-flex">
                     {{-- If route login --}}
                     @if (Route::has('login'))
