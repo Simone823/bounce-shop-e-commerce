506cfd4ba51e8dc49f91087093edf5fbc1e48f98
Simone823 <simone.daglio823@outlook.it>
2022-07-29 15:50:08 +0200

    layout vue

---
+++ resources/js/layouts/layout.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <!-- Layout default -->
+    <div id="layout_default">
+
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: layout,
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
