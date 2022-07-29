13101011615d29407541c19f49cbc11c30899fa9
Simone823 <simone.daglio823@outlook.it>
2022-07-29 16:11:07 +0200

    install vue ui

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,29 +1,27 @@
 import Vue from "vue";
+
 // importo l'istanza router esportata da index.js
 import VueRouter from "vue-router";
 
 Vue.use(VueRouter);
 
-
 // import pages home
-import home from '../pages/home.vue';
-
+import home from "../pages/home.vue";
 
 // array che rotte vue
 const routes = [
     {
-        path: '/',
-        name: 'home',
+        path: "/",
+        name: "home",
         component: home,
     },
 ];
 
-
 //istanza del router
 const router = new VueRouter({
-    mode: 'history',
+    mode: "history",
     routes: routes,
 });
 
 //esporto il router
-export default router
+export default router;
