2d08162a3ae1141a7b495d80dc9bfce1d99b18d8
Simone823 <simone.daglio823@outlook.it>
2022-07-29 15:54:50 +0200

    app vue

---
+++ resources/js/views/App.vue
@@ -0,0 +1,25 @@
+<template>
+
+    <div>
+
+        <!-- Router view -->
+        <router-view></router-view>
+
+    </div>
+
+</template>
+
+<script>
+import home from '../pages/home.vue';
+
+    export default {
+        components: {
+            home,
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
