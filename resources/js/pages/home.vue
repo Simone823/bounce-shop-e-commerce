11564027e1994e2b5bf5c6eae4c43f7bbe9b2824
Simone823 <simone.daglio823@outlook.it>
2022-07-29 15:52:22 +0200

    pages home vue

---
+++ resources/js/pages/home.vue
@@ -0,0 +1,18 @@
+<template>
+
+    <!-- Home -->
+    <div id="home">
+        <h1>HOMEPAGE</h1>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
