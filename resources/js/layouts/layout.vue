20d00e670b05c65658ae8029ac9c34da048a6468
Simone823 <simone.daglio823@outlook.it>
2022-07-30 15:51:01 +0200

    fix layout vue app

--- resources/js/layouts/layout.vue
+++ resources/js/layouts/layout.vue
@@ -1,9 +1,9 @@
 <template>
 
     <!-- App -->
-    <div id="app">
+    <div id="app" class="bg-dark">
 
-
+        <!-- Main slot -->
         <main>
             <slot/>
         </main>
@@ -19,5 +19,30 @@
 </script>
 
 <style lang="scss" scoped>
+
+// App vue
+#app {
+    height: calc(100vh - 75px);
+    color: white;
+
+    // main
+    main {
+        width: 100%;
+        height: 100%;
+        background: url('/img/background_dark.jpg');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        overflow: hidden;
+
+        // section
+        section {
+            width: 100%;
+            height: 100%;
+            padding: 24px 0;
+            overflow-y: auto;
+        }
+    }
+}
 
 </style>
