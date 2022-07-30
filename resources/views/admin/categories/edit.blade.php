4fd4fc4f779bc18aa023c6c7064d0078468b925c
Simone823 <simone.daglio823@outlook.it>
2022-07-30 11:02:25 +0200

    fix height categories

--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -13,7 +13,7 @@
     {{-- section category edit --}}
     <section id="category_edit">
         
-        <div class="container">
+        <div class="container h-100">
             <div class="row justify-content-center align-items-center h-100">
 
                 <div class="col-12 col-sm-8">
