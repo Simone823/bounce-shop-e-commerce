1d0cb88c5f79d67d49b5e0f656eccc6e37a81fa3
Simone823 <simone.daglio823@outlook.it>
2022-08-01 13:51:43 +0200

    router rotta products

---
+++ resources/js/pages/products.vue
@@ -0,0 +1,34 @@
+<template>
+    
+    <!-- layout -->
+    <layout>
+
+        <!-- section products -->
+        <section id="products">
+            <div class="container">
+
+                <div class="row">
+                    <h1>products</h1>
+                </div>
+
+            </div>
+        </section>
+
+    </layout>
+
+</template>
+
+<script>
+import layout from '../layouts/layout.vue';
+
+    export default {
+        name: 'products',
+        components: {
+            layout,
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
