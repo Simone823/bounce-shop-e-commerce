c3b89f85931877c534d0b325cd52fbe018aa2315
Simone823 <simone.daglio823@outlook.it>
2022-08-13 18:48:49 +0200

    view orders index admin

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -12,14 +12,56 @@
     <section id="orders_index">
 
         <div class="container h-100">
-            <div class="row h-100 align-items-center">
-
+            <div class="row h-100 align-items-center gy-5 gx-sm-5 gx-lg-5">
 
                 {{-- If orders lenght 0 --}}
                 @if(count($orders) < 1)
                     <div class="no_orders text-center text-white">
                         <h1>Al momento non ci sono ordini</h1>
                     </div>
+
+                    @else
+                       {{-- Title --}}
+                       <div class="col-12 title text-center text-white">
+                           <h1 class="mb-0">Lista Ordini</h1>
+                       </div>
+
+                        @foreach ($orders as $order)
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <div class="card h-100">
+
+                                    <figure class="wrapper_image shadow bg-dark">
+                                        <img src="{{asset('img/bounce_shop_logo.png')}}" alt="">
+                                    </figure>
+
+                                    <div class="card-body d-flex flex-column justify-content-between gap-3">
+
+                                        {{-- Description --}}
+                                        <div class="description">
+                                            <h5 class="card-title fw-bolder">Ordine n° {{$order->id}}</h5>
+                                            <p class="card-text fs-5">Creato <span>{{\Carbon\Carbon::create($order->created_at)->diffForHumans()}}</span></p>
+                                            <p class="card-text fs-5">{{$order->total_price}} &euro;</p>
+
+                                            <div class="card-text d-flex flex-wrap gap-2">
+                                                <p class="badge bg-success fs-6 fw-light text-uppercase {{$order->status == 1 ? 'opacity-100' : 'opacity-25'}}">Pagato</p>
+                                                <p class="badge bg-danger fs-6 fw-light text-uppercase {{$order->status == 0 ? 'opacity-100' : 'opacity-25'}}">Non Pagato</p>
+                                            </div>
+                                        </div>
+
+                                        {{-- Btn --}}
+                                        <div class="buttons d-flex flex-wrap justify-content-center gap-3">
+                                            <a href="" class="btn btn-primary text-white">Visualizza</a>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        @endforeach
+
+                        {{-- Link paginate --}}
+                        <div class="link_paginate col-12 d-flex justify-content-center">
+                            {{$orders->links()}}
+                        </div>
                 @endif
 
             </div>
