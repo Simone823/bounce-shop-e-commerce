0d26f6b6d8401485d32204441a27346f03b69d08
Simone823 <simone.daglio823@outlook.it>
2022-08-14 21:25:14 +0200

    modifica model order product

--- app/Order.php
+++ app/Order.php
@@ -8,7 +8,7 @@ class Order extends Model
 {
     public function products()
     {
-        return $this->belongsToMany('App\Product');
+        return $this->belongsToMany('App\Product')->withPivot(['quantity']);
     }
 
     protected $fillable = [
