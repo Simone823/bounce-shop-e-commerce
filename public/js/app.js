a745a7d2678e542bc9bcc7de0fb69c4a8e693bc1
Simone823 <simone.daglio823@outlook.it>
2022-10-11 15:42:37 +0200

    run watch

--- public/js/app.js
+++ public/js/app.js
@@ -41198,9 +41198,9 @@ window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Simone\Desktop\Progetti\bounce-shop-e-commerce\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\Simone\Desktop\Progetti\bounce-shop-e-commerce\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\Users\Simone\Desktop\Progetti\bounce-shop-e-commerce\resources\sass\front.scss */"./resources/sass/front.scss");
+__webpack_require__(/*! D:\Progetti\bounce-shop-e-commerce\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! D:\Progetti\bounce-shop-e-commerce\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! D:\Progetti\bounce-shop-e-commerce\resources\sass\front.scss */"./resources/sass/front.scss");
 
 
 /***/ })
