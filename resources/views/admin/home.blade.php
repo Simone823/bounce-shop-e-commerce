91adce807dbb24dec6938c437d819d6d66b5a7c6
Simone823 <simone.daglio823@outlook.it>
2022-10-11 16:35:45 +0200

    admin home add chart js

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,38 +3,78 @@
 @section('metaTitle', '| Admin Dashboard')
 
 @section('header')
-
     @include('components.adminHeader')
-
 @endsection
 
 @section('content')
 
     {{-- section  admin home dashboard --}}
     <section id="admin_home">
-
         <div class="container">
             <div class="row justify-content-center">
 
-                <div class="col-md-8">
-
-                    <div class="card">
-                        <div class="card-header">{{ __('Dashboard Admin') }}</div>
-        
-                        <div class="card-body">
-                            @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                            @endif
-        
-                            {{ __('You are logged in! as Admin') }}
-                        </div>
-                    </div>
+                {{-- user chart bar --}}
+                <div class="col-12">
+                    <canvas id="userChart" class="rounded"></canvas>
                 </div>
 
             </div>
         </div>
-
     </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+
+    <script>
+        var canva_user = document.getElementById('userChart').getContext('2d');
+        var user_chart = new Chart(canva_user, {
+            // The type of chart we want to create
+            type: 'bar',
+            // The data for our dataset
+            data: {
+                labels: {!!json_encode($user_chart->labels)!!},
+                datasets: [{
+                    label: 'Utenti registrati', 
+                    backgroundColor: {!!json_encode($user_chart->colours)!!},
+                    data: {!!json_encode($user_chart->dataset)!!},
+                }]
+            },
+            // Configuration options go here
+            options: {
+                title: {
+                    display: true,
+                    text: 'Utenti registrati al mese',
+                    fontSize: 20,
+                    padding: 15
+                },
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true, 
+                            callback: function(value) {
+                                if (value % 1 === 0) {
+                                    return value;
+                                }
+                            }
+                        }, 
+                        scaleLabel: {
+                            display: false
+                        }
+                    }]
+                }, 
+                legend: {
+                   display: false
+                }, 
+                layout: {
+                    padding: {
+                        left: 10,
+                        right: 10,
+                        top: 0,
+                        bottom: 10
+                    }
+                }
+            }
+        });
+
+    </script>
+
 @endsection
